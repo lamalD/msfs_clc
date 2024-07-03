e5abc63ba09924d210082df5d92abdc3c57522c9
Dries Lamal <dries.lamal@innaviation.com>
2024-07-03 13:54:09 +0200

    Changes to be committed: modified:   app/(auth)/layout.tsx modified:   app/(auth)/sign-in/[[...sign-in]]/page.tsx modified:   app/(auth)/sign-up/[[...sign-up]]/page.tsx modified:   app/(root)/layout.tsx modified:   app/layout.tsx deleted:    app/page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,18 +27,18 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang='en'>
-      <body>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        {children}
-      </body>
-    </html>
-  </ClerkProvider>
+      <html lang='en'>
+        <body>
+          {/* <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
                 
