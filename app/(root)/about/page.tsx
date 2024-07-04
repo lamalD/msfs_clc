695e2ef6bcb902470e58d0e2e98542a371c4626b
Dries Lamal <dries.lamal@innaviation.com>
2024-07-04 17:11:27 +0200

    new file:   app/(root)/about/page.tsx new file:   app/(root)/pricing/page.tsx

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col gap-9'>
+      <section className='flex flex-col gap-5'>
+        <h1 className='text-20 font-bold text-white-1'>About</h1>
+      </section>
+    </div>
+  )
+}
+
+export default About
---
+++ app/(root)/pricing/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Pricing = () => {
+  return (
+    <div className='flex flex-col gap-9'>
+      <section className='flex flex-col gap-5'>
+        <h1 className='text-20 font-bold text-white-1'>Pricing</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Pricing
