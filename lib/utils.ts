5f1e6dd46fee4c8daea5473dcd13be6d669502bd
Dries Lamal <dries.lamal@innaviation.com>
2024-07-09 10:20:34 +0200

    new file:   lib/actions/user.actions.ts 	modified:   lib/utils.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   types/index.d.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-prototype-builtins*/
+
 import { type ClassValue, clsx } from "clsx"
+/*import qs from "qs"*/
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// ERROR HANDLER
+export const handleError = (error: unknown) => {
+  if (error instanceof Error) {
+    console.error(error.message)
+    throw new Error(`Error: ${error.message}`)
+  } else if (typeof error === "string") {
+    console.error(error)
+    throw new Error(`Error: ${error}`)
+  } else {
+    console.error(error)
+    throw new Error(`Unknown error: ${JSON.stringify(error)}`)
+  }
 }
